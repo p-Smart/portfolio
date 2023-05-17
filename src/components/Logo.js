a2ce409f392cc8ea6ad5a407bf83bc4edba03b5a
p-Smart <princeayokunle2002@gmail.com>
2023-05-17 22:22:25 +0100

    Testing Commit

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -8,7 +8,7 @@ function Logo({animate}){
             <Link href="/" className="logo-wrapper">
                 <div className="icon"><FaChevronLeft style={{transform:"rotate(135deg)"}}/></div>
                 <div className="logo">
-                    p<span>S</span>mart
+                    a<span>S</span>mart
                 </div>
                 <div className="icon"><FaChevronLeft style={{transform:"rotate(-45deg)"}}/></div>
             </Link>
