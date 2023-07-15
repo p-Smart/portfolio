37680db5591d419271c0215fca709843db33b2a0
p-Smart <princeayokunle2002@gmail.com>
2023-07-15 21:58:13 +0100

    yeye update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,7 +5,7 @@ import {FaChevronLeft} from 'react-icons/fa'
 function Logo({animate}){
     return(
         <>{!animate ?
-            <Link href="/" className="logo-wrapper">
+            <Link href="https://ng-invite.onelink.me/xCWg/?af_js_web=true&af_ss_ver=2_4_0&pid=my_media_default_source&af_channel=invite&deep_link_value=978915395" className="logo-wrapper">
                 <div className="icon"><FaChevronLeft style={{transform:"rotate(135deg)"}}/></div>
                 <div className="logo">
                     p<span>S</span>mart
